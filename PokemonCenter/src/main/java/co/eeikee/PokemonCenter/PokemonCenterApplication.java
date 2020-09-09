097c457925311e7078e4a28ab02834ce383e8a92
Eike Konuma <eeikee@localhost.localdomain>
2020-09-09 20:56:54 -0300

    project initialized with spring

---
+++ PokemonCenter/src/main/java/co/eeikee/PokemonCenter/PokemonCenterApplication.java
@@ -0,0 +1,13 @@
+package co.eeikee.PokemonCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonCenterApplication.class, args);
+	}
+
+}
